ff2a62793299938fec00e8b13a9b290234b67bd6
IrinaZagaynova <ayrin.zagaynova@gmail.com>
2020-11-08 18:54:35 +0300

    update

---
+++ ArticlesService.Domain/Models/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticlesService.Domain.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public User User { get; set; }
+        public List<ArticleCategory> Categories { get; set; }
+        public List<ArticleImage> Images { get; set; }
+    }
+}
---
+++ ArticlesService.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using ArticlesService.Domain.Interfaces;
+using ArticlesService.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticlesService.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
