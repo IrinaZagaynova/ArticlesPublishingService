4d85b48c562f82d3a832c37c6abb4db4c8203537
IrinaZagaynova <ayrin.zagaynova@gmail.com>
2021-01-11 14:32:31 +0300

    Update requests.sql

--- back/requests.sql
+++ back/requests.sql
@@ -1,49 +1,24 @@
 ﻿--получение краткого описания всех статей
 
-SELECT [a].[id_article] AS [Id], [a].[title] AS [Title], [a].[description] AS [Description], [u].[login] AS [UserLogin]
+SELECT [a].[id_article] AS [Id], [a].[title] AS [Title], [a].[description] AS [Description], 
+[u].[login] AS [UserLogin], [a].[creation-date] AS [CreationDate]
 FROM [article] AS [a]
 INNER JOIN [user] AS [u] ON [a].[id_user] = [u].[id_user]
-ORDER BY [a].[id_article] DESC
-
---получение статьи по id статьи
-
-DECLARE @__articleId_0 int;  
-SET @__articleId_0 = 1
-
-SELECT TOP(2) [a].[id_article] AS [Id], [a].[title] AS [Title], [a].[content] AS [Content], [u].[login] AS [UserLogin]
-FROM [article] AS [a]
-INNER JOIN [user] AS [u] ON [a].[id_user] = [u].[id_user]
-WHERE [a].[id_article] = @__articleId_0
-
---получение комментов по id статьи
-
-DECLARE @__articleId_5 int;  
-SET @__articleId_5 = 1
-
-SELECT [c].[id_comment] AS [Id], [u].[login] AS [Login], [c].[text] AS [Text]
-FROM [comment] AS [c]
-INNER JOIN [user] AS [u] ON [c].[id_user] = [u].[id_user]
-WHERE [c].[id_article] = @__articleId_5
+ORDER BY [a].[creation-date] DESC
 
 --создание статьи
 
-DECLARE @title nvarchar(200);  
-SET @title = N'title'
-DECLARE @description nvarchar(max);  
-SET @description = N'description'
-DECLARE @content nvarchar(max);  
-SET @content = N'content'
-DECLARE @id_user int;  
-SET @id_user = 3
+DECLARE @title nvarchar(200) = N'title';
+DECLARE @description nvarchar(max) = N'description'
+DECLARE @creationdate datetime = '12-21-16 00:00';
+DECLARE @content nvarchar(max) = N'content'
+DECLARE @id_user int = 3;
 
 BEGIN TRANSACTION 
 
 SET NOCOUNT ON;
-INSERT INTO [article] ([content], [description], [title], [id_user])
-VALUES (@description, @content, @title, @id_user);
-SELECT [id_article]
-FROM [article]
-WHERE @@ROWCOUNT = 1 AND [id_article] = scope_identity();
+INSERT INTO [article] ([content], [creation-date], [description], [title], [id_user])
+VALUES (@description, @creationdate, @content, @title, @id_user);
 
 ROLLBACK;
 
@@ -128,28 +103,30 @@ WHERE EXISTS (
 DECLARE @__title_0 nvarchar(200);  
 SET @__title_0 = N'science'
 
-SELECT [a].[id_article] AS [Id], [a].[title] AS [Title], [a].[description] AS [Description], [u].[login] AS [UserLogin]
+SELECT [a].[id_article] AS [Id], [a].[title] AS [Title], [a].[description] AS [Description], [u].[login] AS [UserLogin], [a].[creation-date] AS [CreationDate]
 FROM [article] AS [a]
 INNER JOIN [user] AS [u] ON [a].[id_user] = [u].[id_user]
 WHERE (@__title_0 = N'') OR (CHARINDEX(@__title_0, [a].[title]) > 0)
+ORDER BY [a].[creation-date] DESC
 
 --поиск статей по логину автора
 
 DECLARE @__login_0 nvarchar(50);  
 SET @__login_0 = N'Alice'
 
-SELECT [a].[id_article] AS [Id], [a].[title] AS [Title], [a].[description] AS [Description], [u].[login] AS [UserLogin]
+SELECT [a].[id_article] AS [Id], [a].[title] AS [Title], [a].[description] AS [Description], [u].[login] AS [UserLogin], [a].[creation-date] AS [CreationDate]
 FROM [article] AS [a]
 INNER JOIN [user] AS [u] ON [a].[id_user] = [u].[id_user]
 WHERE (@__login_0 = N'') OR (CHARINDEX(@__login_0, [u].[login]) > 0)
+ORDER BY [a].[creation-date] DESC
 
 --получени статей по id автора
 
 DECLARE @__userId_0 int;  
 SET @__userId_0 = 1
 
-SELECT [a].[id_article] AS [Id], [a].[title] AS [Title], [a].[description] AS [Description]
+SELECT [a].[id_article] AS [Id], [a].[title] AS [Title], [a].[description] AS [Description], [a].[creation-date] AS [CreationDate]
 FROM [article] AS [a]
 INNER JOIN [user] AS [u] ON [a].[id_user] = [u].[id_user]
 WHERE [u].[id_user] = @__userId_0
-ORDER BY [a].[id_article] DESC
+ORDER BY [a].[creation-date] DESC
